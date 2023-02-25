ccac2edb7a799f29df4b2044d97c99e67e0f9a50
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-02-25 12:12:17 +0700

    feat: Add index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+const LinkedList = require('./LinkedList');
+
+const list = new LinkedList();
+// Append
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// console.log(list.list);
+// console.log(list.length);
+
+// prevend
+// list.prepend('head');
+// list.prepend('tail');
+// console.log(list.list);
+// console.log(list.length);
+
+// size;
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// console.log({ size: list.size() === 3 });
+
+// at
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// console.log(list.at(1));
+// console.log(list.at(2));
+
+//pop
+// list.append('tail');
+// list.append('head');
+// console.log(list.list);
+// console.log(list.length);
+// console.log(list.pop());
+// console.log(list.list);
+// console.log(list.length);
+
+// contains
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// console.log(`expect: true, actulal: ${list.contains('tail')}`);
+// console.log(`expect: true, actulal: ${list.contains('header')}`);
+// console.log(`expect: true, actulal: ${list.contains('body')}`);
+// console.log(`expect: false, actulal: ${list.contains('test')}`);
+
+//find
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// console.log(`expect: 2, actulal: ${list.find('tail')}`);
+// console.log(`expect: 0, actulal: ${list.find('header')}`);
+// console.log(`expect: 1, actulal: ${list.find('body')}`);
+// console.log(`expect: null, actulal: ${list.find('test')}`);
+
+// tostring
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// console.log(list.toString());
+
+// insert at
+// list.append('tail');
+// list.append('body');
+// list.append('header');
+// list.insertAt('test', 2);
+// console.log(list.head);
+// console.log(list.length);
+
+// removeAt
+// list.append('tail');
+// list.append('body');
+// // list.append('header');
+// list.removeAt(1);
+// console.log(list.head);
+// console.log(list.length);
